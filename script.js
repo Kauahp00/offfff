f8e16fee7120e95186170d763234282f4d25a243
Kauahp00 <132684345+Kauahp00@users.noreply.github.com>
2025-07-21 19:02:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
- document.addEventListener("DOMContentLoaded", function () {
-    const botaoMobile = document.getElementById("botaoMobile");
-    const menuSusp = document.getElementById("menuSusp");
+const botaoMobile = document.getElementById('botaoMobile');
+const menuSusp = document.getElementById('menuSusp');
 
-    // Garante que o menu começa fechado
-    menuSusp.style.display = "none";
-
-    // Abre e fecha com clique
-    botaoMobile.addEventListener("click", () => {
-      if (menuSusp.style.display === "none" || menuSusp.style.display === "") {
-        menuSusp.style.display = "flex";
-      } else {
-        menuSusp.style.display = "none";
-      }
-    });
-  });
+botaoMobile.addEventListener('click', () => {
+  if (menuSusp.style.display === 'flex') {
+    menuSusp.style.display = 'none';
+  } else {
+    menuSusp.style.display = 'flex';
+  }
+});
